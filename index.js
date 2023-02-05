2af1e627be9d14b48e982bd64e8c105d8fbf7873
Sahil <leviack629@gmail.com>
2023-02-05 17:50:17 +0530

    iife

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const add = require("./add");
+// const add = require("./add");
 
-console.log("Hello from index.js");
-const sum = add(1, 2);
-const sum2 = add(2, 5);
-console.log(sum);
-console.log(sum2);
+// console.log("Hello from index.js");
+// const sum = add(1, 2);
+// const sum2 = add(2, 5);
+// console.log(sum);
+// console.log(sum2);
 
+require("./batman");
+require("./superman");
