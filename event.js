5d47b2fb91563863e6aef5aee1738a3f3f24d937
Sahil <leviack629@gmail.com>
2023-02-06 21:40:56 +0530

    extending from eventEmitter

--- event.js
+++ event.js
@@ -1,15 +1,27 @@
-const EventEmitter = require("node:events")
+// const EventEmitter = require("node:events")
 
-const emitter = new EventEmitter()
+// const emitter = new EventEmitter()
 
 
-emitter.on("order-pizza", (size, topping) => {
-    console.log(`Order received! Baking a ${size} pizza with ${topping}.`)
-})
+// emitter.on("order-pizza", (size, topping) => {
+//     console.log(`Order received! Baking a ${size} pizza with ${topping}.`)
+// })
 
-emitter.on("order-pizza", (size) => {
-    if(size === "large") {
-        console.log("Serving complimentary drink!")
-    }
-})
-emitter.emit("order-pizza", "large", "corn")
+// emitter.on("order-pizza", (size) => {
+//     if(size === "large") {
+//         console.log("Serving complimentary drink!")
+//     }
+// })
+// emitter.emit("order-pizza", "large", "corn")
+
+
+const PizzaShop = require("./pizza-shop")
+
+const pizzaShop = new PizzaShop();
+
+pizzaShop.on("order", (size, topping) => {
+    console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+});
+
+pizzaShop.order("large", "corn")
+pizzaShop.displayOrderNumber()
