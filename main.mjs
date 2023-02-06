9b52a565a37a92cece0469978e8a739b993ba05e
Sahil <leviack629@gmail.com>
2023-02-06 15:29:07 +0530

    esm

--- main.mjs
+++ main.mjs
@@ -2,9 +2,14 @@
 
 // console.log(add(2, 4));
 
-import math from "./math-esm.mjs";
+// import math from "./math-esm.mjs";
 
 // const {add, subtract} = math;
 
-console.log(math.add(5, 5));
-console.log(math.subtract(6, 5));
+
+// import * as math from "./math-esm.mjs";
+
+import {add, subtract} from "./math-esm.mjs"
+
+console.log(add(5, 5));
+console.log(subtract(6, 5));
--- math-esm.mjs
+++ math-esm.mjs
@@ -11,16 +11,25 @@
 // };
 
 
-const add = (a, b) => {   //3
+// const add = (a, b) => {   //3
+//     return a + b;
+// };
+
+// const subtract = (a, b) =>{
+//     return a - b;
+// };
+
+// export default {
+//     add, 
+//     subtract
+// };
+
+
+
+export const add = (a, b) => {   //4
     return a + b;
 };
 
-const subtract = (a, b) =>{
+export const subtract = (a, b) =>{
     return a - b;
 };
-
-export default {
-    add, 
-    subtract
-};
-
