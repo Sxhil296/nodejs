bbdfa155ff7b026673b680cfeee7322511833c25
Sahil <leviack629@gmail.com>
2023-02-07 10:40:43 +0530

    fsModule

--- fsModule.js
+++ fsModule.js
@@ -1,13 +1,51 @@
-const fs = require("node:fs")
+const fs = require("node:fs/promises")
 
-const fileContent = fs.readFileSync("./file.txt", "utf-8")
 
-console.log(fileContent)
-
-fs.readFile("./file.txt", "utf-8", (error, data) => {
-    if(error) {
-        console.log(error)
-    } else {
+async function readFile() {
+    try {
+        const data = await fs.readFile("file.txt", "utf-8")
         console.log(data)
+    } catch(err) {
+        console.log(err)
     }
-})
+}
+
+readFile()
+
+// console.log('first')
+
+// fs.readFile("file.txt", "utf-8")
+// .then((data) => console.log(data))
+// .catch((error) => console.log(error))
+
+// console.log("second")
+
+
+
+
+// const fs = require("node:fs")
+
+// console.log("first")
+// const fileContent = fs.readFileSync("./file.txt", "utf-8")
+// console.log(fileContent)
+
+// console.log('second')
+
+// fs.readFile("./file.txt", "utf-8", (error, data) => {
+//     if(error) {
+//         console.log(error)
+//     } else {
+//         console.log(data)
+//     }
+// })
+
+// console.log("third")
+
+// fs.writeFileSync("./greet.txt", "hello world")
+// fs.writeFile("./greet.txt", " hello sahil", {flag: "a"}, (err) => {
+//     if(err) {
+//         console.log(err)
+//     } else {
+//         console.log("file written")
+//     }
+// })
