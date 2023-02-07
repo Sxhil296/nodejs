065e1d35281691680630c60373c3936bc8f5569f
Sahil <leviack629@gmail.com>
2023-02-07 12:21:45 +0530

    html response

--- htmlResponse.js
+++ htmlResponse.js
@@ -9,10 +9,12 @@ const server = http.createServer((req, res) => {
         lastName: "Wayne"
     }
 
+    const name = "Sahil"
     res.writeHead(200, {"Content-Type": "text/html"})
-    fs.createReadStream(__dirname + "/index.html").pipe(res)
-    // const html = fs.readFileSync("./index.html", "utf-8")
-    // res.end(html)
+    // fs.createReadStream(__dirname + "/index.html").pipe(res)
+    let html = fs.readFileSync("./index.html", "utf-8")
+    html = html.replace("{{name}}", name)
+    res.end(html)
 })
 
 server.listen(3000, () =>{
